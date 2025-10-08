6f8b0d767bf7e2eb0cce2cd9b63f7085bf1523f7
crivella <davidecrivella@yahoo.it>
2025-10-08 16:30:22 +0200

    Placeholder for the CLI

---
+++ aitw_aiida_viscosity/cli/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=wrong-import-position
+"""Module for the command line interface."""
+from aiida.cmdline.groups import VerdiCommandGroup
+from aiida.cmdline.params import options, types
+import click
+
+
+@click.group('aitw-aiida-viscosity', cls=VerdiCommandGroup, context_settings={'help_option_names': ['-h', '--help']})
+@options.PROFILE(type=types.ProfileParamType(load_profile=True), expose_value=False)
+def cmd_root():
+    """CLI for the `AITW-aiida-viscosity` plugin."""
+
+@cmd_root.command()
+def hello():
+    """Print a hello world message."""
+    click.echo('Hello, AITW-aiida-viscosity!')
