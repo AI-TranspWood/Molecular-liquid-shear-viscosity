5bd7e8316cf41edf4e43865a4a1fe5d67a7a0c2e
crivella <davidecrivella@yahoo.it>
2025-10-09 10:15:34 +0200

    rich_click tracebacks

--- aitw_aiida_viscosity/cli/__init__.py
+++ aitw_aiida_viscosity/cli/__init__.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=wrong-import-position
 """Module for the command line interface."""
+import aiida
+# from aiida.cmdline.groups import VerdiCommandGroup as _VerdiCommandGroup
 from aiida.cmdline.groups import VerdiCommandGroup
 from aiida.cmdline.params import options, types
-import click
+import click as original_click
+import plumpy
+from rich.traceback import install
+import rich_click as click
 
+install(
+    suppress=[original_click, click, aiida, plumpy]
+)
 
-@click.group('aitw-aiida-viscosity', cls=VerdiCommandGroup, context_settings={'help_option_names': ['-h', '--help']})
+@click.group(
+    'aitw-aiida-viscosity',
+    cls=VerdiCommandGroup,
+    context_settings={'help_option_names': ['-h', '--help']}
+)
 @options.PROFILE(type=types.ProfileParamType(load_profile=True), expose_value=False)
 def cmd_root():
     """CLI for the `AITW-aiida-viscosity` plugin."""
@@ -17,3 +29,8 @@ def hello():
     click.echo('Hello, AITW-aiida-viscosity!')
 
 from .workflows import cmd_workflow
+
+__all__ = (
+    'cmd_root',
+    'cmd_workflow',
+)
