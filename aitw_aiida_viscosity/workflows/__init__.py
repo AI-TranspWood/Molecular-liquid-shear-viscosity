039d217f2d9500fb0055a596450d757abb2b577e
crivella <davidecrivella@yahoo.it>
2025-12-02 15:25:46 +0100

    Also expose the NEMD workchain

--- aitw_aiida_viscosity/workflows/__init__.py
+++ aitw_aiida_viscosity/workflows/__init__.py
@@ -1,5 +1,6 @@
 from .GromacsEquilibration import GromacsEquilibrationWorkChain
+from .GromacsNEMD import GromacsNEMDWorkChain
 from .MonomerWorkChain import MonomerWorkChain
 from .RespCharges import RespChargesWorkChain
 
-__all__ = ('MonomerWorkChain', 'RespChargesWorkChain', 'GromacsEquilibrationWorkChain')
+__all__ = ('MonomerWorkChain', 'RespChargesWorkChain', 'GromacsEquilibrationWorkChain', 'GromacsNEMDWorkChain')
